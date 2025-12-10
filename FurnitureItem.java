d4e2345814547c25c6a6c71884d9c7c1526ab81b
giovanni-boulis <jovyboulis69@gmail.com>
2025-12-10 15:53:18 +0200

    Add files via upload

--- FurnitureItem.java
+++ FurnitureItem.java
@@ -1,22 +1,80 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.ecommerceproject;
+
+package com.mycompany.furnitureitem;
 
 /**
  *
- * @author Eng. Youssef
+ * @author giova
  */
-public class FurnitureItem {
-    private String name;
-    private double price;
+public abstract class FurnitureItem implements Comparable <FurnitureItem> {
+    protected int itemid;
+    protected String name;
+    protected double price;
+    protected String material;
+    protected int stock;
 
-    public FurnitureItem(String name, double price) {
+        public FurnitureItem(int itemid, String name, double price, String material, int stock) {
+        this.itemid = itemid;
         this.name = name;
+        this.price = price;
+        this.material = material;
+        this.stock = stock;
+        }
+
+    public int getItemid() {
+        return itemid;
+    }
+
+    public void setItemid(int itemid) {
+        this.itemid = itemid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 
-    public String getName() { return name; }
-    public double getPrice() { return price; }
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
+    @Override    
+     public int compareTo(FurnitureItem other) {                   
+        return Double.compare(this.price, other.price);
+    }
+     
+    @Override
+
+    public String toString() {
+        return "FurnitureItem{" + "itemid=" + itemid + ", name=" + name + ", price=" + price + ", material=" + material + ", stock=" + stock + '}';
+    }
+    
+
 }
+    
+    
